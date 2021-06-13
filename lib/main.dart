387ee2eb46e0b20e351946e6a994c8c2bb376325
macksim288 <den13072002@gmail.com>
2021-06-13 16:42:27 +0300

    flutter_quiz

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'quiz App',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Тестирование'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Рататуй'),
+        ),
+      ),
+    );
+  }
+}
